7ed59e7a154be42854388d562c978daa96062454
Axroxx <coolexander@gmail.com>
2023-03-17 17:53:04 +0100

    html css integration

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        number = request.form['number']
+        try:
+            number = int(number)
+            if number < 1 or number > 100:
+                raise ValueError()
+            return render_template('index.html', result=number)
+        except ValueError:
+            pass
+    return render_template('index.html', result=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
