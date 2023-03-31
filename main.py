f699694535118d0a5e32236133b2df9bbd8f139c
Axroxx <coolexander@gmail.com>
2023-03-31 16:58:43 +0200

    fixes

--- main.py
+++ main.py
@@ -24,14 +24,19 @@ words = [
 def insert(hive):
     permutations = []
     size = hive.complete_amt
-    uncompleted = True
+    
+    for j in range(25):            
+        if not hive.all[j].complete: 
+            if not hive.all[j].empty:
+                if hive.all[j].impossible(hive.avalable):
+                    return []
+    
     for i in range(25):
         if not hive.all[i].complete: 
             if not hive.all[i].empty:
                 for word in hive.avalable:
                     hive_copy = copy.deepcopy(hive)
                     if hive_copy.all[i].insertWord(word):
-                        uncompleted = False
                         hive_copy.updatehexes()
                         hive_copy.used.append(word)
                         hive_copy.avalable.remove(word)
@@ -42,7 +47,7 @@ def insert(hive):
                     else:
                         hive.avalable.remove(word)
                         hive.badlist.append(word)
-            
+        
     nl = []
     
     
@@ -67,13 +72,6 @@ def run(hives):
         
     return permutations             
 
-"""
-def cleanup(i,permutations):    
-    newpermutations = []
-    for per in permutations:
-        if per.complete_amt > i:
-            newpermutations.append(per)
-    return newpermutations"""
 
 def flatten(lst):
     result = []
@@ -112,7 +110,7 @@ def main():
     
     
     
-    for i in range(0,5):  
+    for i in range(26):  
         permutations = []       
         
         ret = run(tree[i])
@@ -143,144 +141,3 @@ def main():
     
     
 main()
-
-
-
-"""
-def run():
-    global biggest_hive, permutations, totalpermu, all_time_biggest
-    
-    newPermutations = []
-    
-    for hive in permutations:
-        print(len(permutations))
-        ins = False
-        if hive.size() == biggest_hive:
-            for word in hive.avalable:
-                if word not in hive.dontcheck:
-                    
-                    inserted = False
-                    for j in range(1,26):
-                        
-                        if not hive[j].complete and not hive[j].empty:
-                            
-                            hive_copy = copy.deepcopy(hive)
-                            
-                            if hive_copy[j].insertWord(word):
-                                
-                                update(hive_copy)
-                                hive_copy.used.append(word)
-                                hive_copy.avalable.remove(word)
-                                for item in hive_copy.dontcheck:
-                                    hive.avalable.append(item)
-                                hive_copy.dontcheck = []
-                                                                
-                                permutations.append(hive_copy)
-                                
-                                ins = True
-                                inserted = True
-                                
-                                if biggest_hive < hive_copy.size():
-                                    biggest_hive = hive_copy.size()
-                                
-                                
-                                if hive_copy.size() > all_time_biggest:
-                                    print(hive_copy)
-                                    all_time_biggest += 1
-                                print(hive_copy)
-                                        
-                    if not inserted:
-                        hive.dontcheck.append(word)
-                    hive.avalable.remove(word)
-                
-                       
-        if hive.avalable == [] or ins == False:
-            permutations.remove(hive)      
-    
-    
-    if h.size() > biggest_hive:
-        biggest_hive == hive         
-    
-    for p in newPermutations:
-        permutations.append(p)
-    
-    return False     """
-
-
-
-"""
-
-
-def run():
-    global biggest_hive, permutations, totalpermu,all_time_biggest
-    new_permutations = False
-
-    if not permutations:
-        print("Largest hive found:")
-        print(all_time_biggest)
-        return True
-    
-    bigger = False
-    
-    for hive in permutations:
-        ins = False
-        if hive.size() == biggest_hive:
-            for word in hive.avalable:
-                
-                inserted = False    
-                if word not in hive.dontcheck:
-                    for j in range(1, 26):
-                        
-                        if not hive[j].complete and not hive[j].empty: # check if location is empty or full
-                                                    
-                            hive_copy = copy.deepcopy(hive)
-
-                            if hive_copy[j].insertWord(word): # if insertion is successfull
-                            
-                        
-                                update(hive_copy)
-                                hive_copy.used.append(word)
-                                
-                                permutations.append(hive_copy)
-                                new_permutations = True
-                                totalpermu += 1
-                                inserted = True
-                                ins = True
-                                bigger = True
-                                
-                                for s in hive.dontcheck:
-                                    hive.avalable.append(s)
-                                hive.dontcheck = []
-                                
-                                
-                                if hive_copy.size() > all_time_biggest:
-                                    all_time_biggest = hive_copy.size()
-                                
-                                if hive_copy.size() > biggest_hive:
-                                    biggest_hive = hive_copy.size()
-                                
-                                
-                            if hive_copy.size() == 24:
-                                print(hive_copy)
-                                print(totalpermu)
-                                return 1
-                                
-                if not inserted:
-                    hive.dontcheck.append(word)
-                hive.avalable.remove(word)
-                
-            if hive.avalable == [] or ins == False:
-                permutations.remove(hive)
-        
-            
-            print(len(permutations), "       ", biggest_hive+1)
-        
-            
-    if not bigger:
-        biggest_hive -= 1
-    
-    if not new_permutations:
-        print(all_time_biggest)
-        return
-    
-"""
