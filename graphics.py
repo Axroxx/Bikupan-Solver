07dd42b482176874599649c4934b3ef659aafefd
axroxx <coolexander@gmail.com>
2023-07-05 18:49:17 +0200

    update

--- graphics.py
+++ graphics.py
@@ -1,27 +1,29 @@
 import tkinter as tk
+import Image
 
 class ImageButton(tk.Button):
     def __init__(self, id, master, command=None):
         self.id = id
-        self.photo = tk.PhotoImage(file='images/Hex.png')
+        self.photo = Image.open('images/Hex.png')
         super().__init__(master, image=self.photo, command=self.on_button_click, borderwidth=0)
         self.pack()
         self.key = ""
         self.canvas = None  # Initialize the canvas attribute as None
 
     def on_button_click(self):
-        print(f"Button {self.id} clicked!")
+        print(f"Button {self.id} clicked!") 
         self.master.bind("<KeyPress>", self.on_key_press)
 
     def on_key_press(self, event):
         
-        if not event.keysym.isalpha() and event.keysym.capitalize() not in ["Å","Ä","Ö"]:  # Check if the pressed key is "Esc" or "BackSpace"
+        if (not event.keysym.isalpha() and event.keysym.capitalize() not in ["Adiaeresis","Aring","Odiaeresis"]) or len(event.keysym) > 1:  # Check if the pressed key is Alpha
             self.key = ""  # Clear the stored key
-            self.photo = tk.PhotoImage(file='images/Hex.png')
+            self.photo = Image.open('images/Hex.png')
             self.config(image=self.photo)
             return
         else:
             self.key = event.keysym.capitalize().strip()
+            print(self.key)
         print(f"Key pressed: {self.key}")
         self.master.unbind("<KeyPress>")
         self.master.focus_set()  # Set focus back to the main window after capturing the key-press
@@ -33,19 +35,41 @@ class ImageButton(tk.Button):
 # Create the main window
 win = tk.Tk()
 win.title("Bikupan")
-win.geometry("800x600")  # Set the initial window size (width x height)
+win.geometry("800x1000")  # Set the initial window size (width x height)
 
-canvas = tk.Canvas(win, width=800, height=600, highlightthickness=0)
+canvas = tk.Canvas(win, width=1000, height=800, highlightthickness=0)
 canvas.pack(fill="both", expand=True)
 
 # Make the window resizable in both directions
 win.resizable(True, True)
 
 # Replace 'path_to_image.png' with the actual path to your image file
-image_button = ImageButton(1, win)
-image_button.canvas = canvas  # Store the canvas reference in the ImageButton object
 
-image_button.place(x=50, y=50)
+hex_buttons = []
+
+for i in range(12):
+    # Create ImageButton instances and add them to the list
+    mult = 0
+    if i > 3:
+        mult = 1
+    if i > 7:
+        mult = 2
+        
+    hex_button = ImageButton(i+1, win, 'images/Hex.png')
+    hex_button.canvas = canvas  # Store the canvas reference in the ImageButton object
+    hex_button.place(x=(100+145*(i%4)), y=(50+85*mult))
+    hex_buttons.append(hex_button)
+    
+for i in range(10):
+    
+    mult = 0
+    if i > 4:
+        mult = 1
+    hex_button = ImageButton(i+1, win, 'images/Hex.png')
+    hex_button.canvas = canvas  # Store the canvas reference in the ImageButton object
+    hex_button.place(x=(28+145*(i%5)), y=(94+85*mult))
+    hex_buttons.append(hex_button)
+
 
 # Start the main event loop
 win.mainloop()
